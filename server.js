9d56cd065bd0c48585d3139fe63b884e38959e03
vamshikrishna <kvamshi@metricvisio.com>
2025-06-16 11:34:21 +0530

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const router=express.Router()
 //Middlewares
 app.use(cors())
 app.use(express.json());
-app.use(express.static("public"));
+app.use('/uploads', express.static('public/uploads'));
 
 
 // Load all routes (just one line!)
